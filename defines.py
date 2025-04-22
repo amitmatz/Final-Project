3a80682e6a5150f965a521da90a38cf2c22b8315
OrAtias99 <or666atias@gmail.com>
2025-04-22 15:30:47 +0300

    files update

---
+++ defines.py
@@ -0,0 +1,12 @@
+# defines.py
+
+import os
+
+# נתיב בסיס לקבצים - תעדכן אם תזיז את הפרויקט
+BASE_DATA_PATH = "G:/My Drive/FinalProject/Data/"
+
+# נתיב לקובץ הקונפיגורציה של הפציינטים
+PATIENTS_CONFIG_PATH = os.path.join("config", "patients_config.json")
+
+# היכן לשמור קבצי פלט של דאטה מעובד
+PROCESSED_DATA_DIR = "processed_data/"
